acf500ed209056ca2a95e76c3a0bba24f674fa6f
crack-a-deal <savkin.andrey03@mail.ru>
2026-02-24 22:05:15 +0300

    Added base condition class

---
+++ Assets/Scripts/Core/Conditions/BaseCondition.cs
@@ -0,0 +1,51 @@
+public abstract class BaseCondition : ICondition
+{
+    private readonly string _id;
+    public string Id => _id;
+
+    private ConditionState _state = ConditionState.Inactive;
+
+    public ConditionState State
+    {
+        get
+        {
+            return _state;
+        }
+        set
+        {
+            if (_state != value)
+            {
+                _state = value;
+            }
+        }
+    }
+
+    public BaseCondition(string id)
+    {
+        _id = id;
+    }
+
+    public void Activate()
+    {
+        State= ConditionState.Pending;
+    }
+
+    public void Deactivate()
+    {
+        State = ConditionState.Inactive;
+    }
+
+    public void Tick(float deltaTime)
+    {
+        if(_state!= ConditionState.Pending)
+        {
+            return;
+        }
+        if (CheckCondition())
+        {
+            State = ConditionState.Met;
+        }
+    }
+
+    protected abstract bool CheckCondition();
+}
--- Assets/Scripts/Core/Conditions/BoolCondition.cs
+++ Assets/Scripts/Core/Conditions/BoolCondition.cs
@@ -1,57 +1,16 @@
-using UnityEngine;
-
-public class BoolCondition : ICondition
+public class BoolCondition : BaseCondition
 {
-    private readonly string _id;
     private readonly bool _expected;
     private readonly Tag<bool> _tag;
 
-    public string Id => _id;
-
-    private ConditionState _state = ConditionState.Inactive;
-
-    public ConditionState State
+    public BoolCondition(string id, Tag<bool> tag, bool expected) : base(id)
     {
-        get => _state;
-        set
-        {
-            if (_state != value)
-            {
-                _state = value;
-            }
-        }
-    }
-
-    public BoolCondition(string id, Tag<bool> tag, bool expected)
-    {
-        _id = id;
         _tag = tag;
         _expected = expected;
     }
 
-    public void Activate()
+    protected override bool CheckCondition()
     {
-        State = ConditionState.Pending;
-        Debug.Log($"{_id}: [{State}]");
-    }
-
-    public void Deactivate()
-    {
-        State = ConditionState.Inactive;
-        Debug.Log($"{_id}: [{State}]");
-    }
-
-    public void Tick(float dt)
-    {
-        if (State != ConditionState.Pending)
-        {
-            return;
-        }
-
-        if (_tag.Value == _expected)
-        {
-            State = ConditionState.Met;
-            Debug.Log($"{_id}: [{State}]");
-        }
+        return _tag.Value == _expected;
     }
 }
